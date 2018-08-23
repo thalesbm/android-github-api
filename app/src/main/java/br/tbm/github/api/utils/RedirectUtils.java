9184409d3baf276782844baf727d7e4bb15f4f25
Thales Marega <thales.bm@hotmail.com>
2018-08-23 13:04:29 +0100

    adicionado icone + splashscreen + alterado as cores do app

---
+++ app/src/main/java/br/tbm/github/api/utils/RedirectUtils.java
@@ -0,0 +1,19 @@
+package br.tbm.github.api.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import br.tbm.github.api.activities.MainActivity;
+
+public class RedirectUtils {
+
+    /**
+     * Metodo responsavel por redirecionar para a tela principal e finalizar a atual tela do aplicativo
+     * @param activity Activity
+     */
+    public static void redirectToMainActivity(Activity activity) {
+        Intent intent = new Intent(activity, MainActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+}
